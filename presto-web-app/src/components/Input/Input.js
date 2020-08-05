92f012875db2bea214f09953d1c0f51d70ca5a9e
Austin Theriot <austinmtheriot@gmail.com>
2020-08-05 17:16:53 -0500

    added htmlFor, id, and autoComplete to input

--- presto-web-app/src/components/Input/Input.js
+++ presto-web-app/src/components/Input/Input.js
@@ -3,85 +3,88 @@ import styles from './Input.module.css';
 import SuggestionList from '../../components/SuggestionList/SuggestionList';
 
 export default (props) => {
-  const [state, setState] = useState({
-    innerType: 'password',
-  });
+	const [state, setState] = useState({
+		innerType: 'password',
+	});
 
-  const togglePasswordVisibility = () => {
-    setState((prevState) => ({
-      innerType: prevState.innerType === 'password' ? 'text' : 'password',
-    }));
-  };
+	const togglePasswordVisibility = () => {
+		setState((prevState) => ({
+			innerType: prevState.innerType === 'password' ? 'text' : 'password',
+		}));
+	};
 
-  return (
-    <>
-      <div className={styles.div}>
-        {props?.customType === 'password' ? (
-          <img
-            className={styles.eye}
-            alt='show password'
-            src={require('../../assets/images/eye.svg')}
-            onClick={togglePasswordVisibility}
-          />
-        ) : null}
-        <label
-          className={[
-            //general
-            styles.label,
+	return (
+		<>
+			<div className={styles.div}>
+				{props?.customType === 'password' ? (
+					<img
+						className={styles.eye}
+						alt='show password'
+						src={require('../../assets/images/eye.svg')}
+						onClick={togglePasswordVisibility}
+					/>
+				) : null}
+				<label
+					htmlFor={props.label}
+					className={[
+						//general
+						styles.label,
 
-            //animate up?
-            props?.inputs[props.customType]?.animateUp
-              ? styles.up
-              : styles.down,
+						//animate up?
+						props?.inputs[props.customType]?.animateUp
+							? styles.up
+							: styles.down,
 
-            //error?
-            props?.inputs[props.customType]?.message?.error
-              ? styles.redLabel
-              : '',
+						//error?
+						props?.inputs[props.customType]?.message?.error
+							? styles.redLabel
+							: '',
 
-            //inactive?
-            props?.readOnly ? styles.inactiveLabel : '',
-          ].join(' ')}>
-          {props?.label || 'Label'}
-        </label>
-      </div>
-      <input
-        readOnly={props?.readOnly || false}
-        className={[
-          //general
-          styles.input,
+						//inactive?
+						props?.readOnly ? styles.inactiveLabel : '',
+					].join(' ')}>
+					{props?.label || 'Label'}
+				</label>
+			</div>
+			<input
+				autoComplete='on'
+				id={props.label}
+				readOnly={props?.readOnly || false}
+				className={[
+					//general
+					styles.input,
 
-          //animate up?
-          props?.inputs[props.customType]?.animateUp ? styles.colorInput : '',
+					//animate up?
+					props?.inputs[props.customType]?.animateUp ? styles.colorInput : '',
 
-          //error?
-          props?.inputs[props.customType]?.message?.error
-            ? styles.redInput
-            : '',
+					//error?
+					props?.inputs[props.customType]?.message?.error
+						? styles.redInput
+						: '',
 
-          //inactive?
-          props?.readOnly ? styles.inactiveInput : '',
-        ].join(' ')}
-        value={props?.inputs[props.customType]?.value || ''}
-        type={
-          props?.type === 'password' ? state.innerType : props?.type || 'text'
-        }
-        onBlur={(e) => props.handleBlur(e, props.customType)}
-        onFocus={(e) => props.handleFocus(e, props.customType)}
-        onChange={(e) => props.handleChange(e, props.customType)}
-      />
-      <SuggestionList
-        suggestions={props?.inputs[props.customType]?.suggestions || null}
-        suggestionClickHandler={props?.suggestionClickHandler || null}
-      />
-      <p
-        className={
-          props?.inputs[props.customType]?.message?.error
-            ? styles.redMessage
-            : styles.message
-        }>
-        {props?.inputs[props.customType]?.message?.text}
-      </p>
-    </>
-  );
+					//inactive?
+					props?.readOnly ? styles.inactiveInput : '',
+				].join(' ')}
+				value={props?.inputs[props.customType]?.value || ''}
+				type={
+					props?.type === 'password' ? state.innerType : props?.type || 'text'
+				}
+				onBlur={(e) => props.handleBlur(e, props.customType)}
+				onFocus={(e) => props.handleFocus(e, props.customType)}
+				onChange={(e) => props.handleChange(e, props.customType)}
+			/>
+			<SuggestionList
+				suggestions={props?.inputs[props.customType]?.suggestions || null}
+				suggestionClickHandler={props?.suggestionClickHandler || null}
+			/>
+			<p
+				className={
+					props?.inputs[props.customType]?.message?.error
+						? styles.redMessage
+						: styles.message
+				}>
+				{props?.inputs[props.customType]?.message?.text}
+			</p>
+		</>
+	);
 };
