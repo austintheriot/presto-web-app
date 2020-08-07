ee6d34b10009b98367cc7fa5a6e8ed57d773188d
Austin Theriot <austinmtheriot@gmail.com>
2020-08-07 01:32:07 -0500

    configured app to retrieve user data before completing init upon log in

--- presto-web-app/src/App.js
+++ presto-web-app/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { firebaseAuth } from './util/config';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 
 //pages
 import LoadingScreen from './Pages/LoadingScreen/LoadingScreen';
@@ -18,10 +20,10 @@ import Settings from './Pages/Settings/Settings';
 //components
 import PrivateRoute from './components/PrivateRoute';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  withRouter,
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	withRouter,
 } from 'react-router-dom';
 
 //context
@@ -42,90 +44,133 @@ import "firebase/remote-config"; */
 /* const db = firebase.firestore(); */
 
 function App() {
-  const [user, setUser] = useState(false);
+	const [user, setUser] = useState(false);
 
-  /* const fakeDelay = (delayTime) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => resolve(), delayTime);
-    });
+	/* 	const fakeDelay = (delayTime) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => resolve(), delayTime);
+		});
   }; */
+	/* fakeDelay(5000).then(() => {}) */
 
-  useEffect(() => {
-    /* fakeDelay(0).then(() => {
-      //put authListener in here to see loading screen
-    }); */
-    authListener();
-    console.log('[App.js] useEffect is firing...');
-  }, []);
+	useEffect(() => {
+		authListener();
+		console.log('[App.js] useEffect is firing...');
+	}, []);
 
-  function authListener() {
-    firebaseAuth.onAuthStateChanged((user) => {
-      if (user) {
-        console.log('[App]: user logged in');
-        setUser({
-          authenticated: true,
-          init: true,
-          email: user.email,
-          uid: user.uid,
-          displayName: user.displayName,
-          emailVerified: user.emailVerified,
-          photoUrl: user.photoURL,
-          isAnonymous: user.isAnonymous,
-        });
-      } else {
-        setUser({
-          authenticated: false,
-          init: true,
-          email: null,
-          uid: null,
-          displayName: null,
-          emailVerified: null,
-          photoUrl: null,
-          isAnonymous: null,
-        });
-      }
-    });
-    //second argument here causes to never run again (since the array doesn't change)
-  }
-  return (
-    <AuthContext.Provider value={user}>
-      <Router>
-        <div className='App'>
-          {user.init ? (
-            <>
-              <Switch>
-                <Route exact path='/' component={HomePublic} />
-                <Route exact path='/logout' component={LogoutByRender} />
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/signup' component={Signup} />
-                <PrivateRoute
-                  exact
-                  path='/signup/personal'
-                  component={SignupPersonal}
-                />
-                <PrivateRoute
-                  exact
-                  path='/signup/location'
-                  component={SignupLocation}
-                />
-                <PrivateRoute
-                  exact
-                  path='/signup/profile'
-                  component={SignupProfile}
-                />
-                <PrivateRoute exact path='/posts' component={Posts} />
-                <PrivateRoute exact path='/profile' component={Profile} />
-                <PrivateRoute exact path='/settings' component={Settings} />
-                <Route path='*' component={HomePrivate} />
-              </Switch>
-            </>
-          ) : (
-            <LoadingScreen />
-          )}
-        </div>
-      </Router>
-    </AuthContext.Provider>
-  );
+	function authListener() {
+		firebaseAuth.onAuthStateChanged((user) => {
+			if (user) {
+				let authenticationData = user;
+				//get data from authentication request
+				let {
+					email,
+					uid,
+					displayName,
+					emailVerified,
+					photoUrl,
+					isAnonymous,
+				} = user;
+				console.log('[App]: user logged in');
+				//get user data from database
+				firebase
+					.firestore()
+					.collection('users')
+					.doc(authenticationData.uid)
+					.get()
+					.then((doc) => {
+						let {
+							activity = '',
+							bio = '',
+							city = '',
+							country = '',
+							county = '',
+							instrument = '',
+							name = '',
+							state = '',
+							type = '',
+							website = '',
+							zip = '',
+						} = doc.data();
+
+						let allUserInfo = {
+							authenticated: true,
+							init: true,
+							email,
+							uid,
+							displayName,
+							emailVerified,
+							photoUrl,
+							isAnonymous,
+							activity,
+							bio,
+							city,
+							country,
+							county,
+							instrument,
+							name,
+							state,
+							type,
+							website,
+							zip,
+						};
+
+						console.log(allUserInfo);
+						//update user data on the client side with authentication & database data
+						//only show full screen once user info has been successfully retrieved
+						setUser(allUserInfo);
+					})
+					.catch((err) => {
+						console.error(err);
+					});
+			} else {
+				//replace all user data with empty object
+				//BUT still tell app that everything is initialized
+				setUser({ init: true });
+			}
+		});
+		//second argument here causes to never run again (since the array doesn't change)
+	}
+	return (
+		<AuthContext.Provider value={user}>
+			<Router>
+				<div className='App'>
+					{user.init ? (
+						<>
+							<Switch>
+								<Route exact path='/' component={HomePublic} />
+								<Route exact path='/logout' component={LogoutByRender} />
+								<Route exact path='/login' component={Login} />
+								<Route exact path='/signup' component={Signup} />
+								<PrivateRoute
+									exact
+									path='/signup/personal'
+									component={SignupPersonal}
+								/>
+								<PrivateRoute
+									exact
+									path='/signup/location'
+									component={SignupLocation}
+								/>
+								<PrivateRoute
+									exact
+									path='/signup/profile'
+									component={SignupProfile}
+								/>
+								<PrivateRoute exact path='/home' component={HomePrivate} />
+								<PrivateRoute exact path='/posts' component={Posts} />
+								<PrivateRoute exact path='/profile' component={Profile} />
+								<PrivateRoute exact path='/settings' component={Settings} />
+								<Route path='*' component={HomePublic} />
+							</Switch>
+						</>
+					) : (
+						<LoadingScreen />
+					)}
+				</div>
+			</Router>
+		</AuthContext.Provider>
+	);
 }
 
 //wrapping the App in a higher order component
@@ -135,11 +180,11 @@ const AppWithRouter = withRouter(App);
 
 //surrounding app with Router
 const AppContainer = () => {
-  return (
-    <Router>
-      <AppWithRouter />
-    </Router>
-  );
+	return (
+		<Router>
+			<AppWithRouter />
+		</Router>
+	);
 };
 
 export default AppContainer;
