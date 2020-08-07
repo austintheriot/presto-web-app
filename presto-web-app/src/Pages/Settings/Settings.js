47e470721c49ce090d0bd9c92daef1b5c0ba8eb1
Austin Theriot <austinmtheriot@gmail.com>
2020-08-07 15:04:06 -0500

    added logout to settings

--- presto-web-app/src/Pages/Settings/Settings.js
+++ presto-web-app/src/Pages/Settings/Settings.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import Nav from '../../components/Nav/Nav';
+import styles from './Settings.module.css';
+import Button from '../../components/Button/Button';
+import Logout from '../../components/Logout';
+import logoutFunction from '../../util/logout';
+
+//images
+import logout from '../../assets/images/logout.svg';
+import notifications from '../../assets/images/notifications.svg';
 
 export default (props) => {
-  return (
-    <>
-      <Nav />
-      <h1>Settings</h1>
-    </>
-  );
+	return (
+		<>
+			<Nav />
+			<h1 className={styles.title}>Account Settings</h1>
+			<Button>Save</Button>
+			{/* LOG OUT */}
+			<div className={styles.wrapper}>
+				<div className={styles.logoutButtonDiv}>
+					<img src={logout} alt='logout' onClick={logoutFunction}></img>
+					<p onClick={logoutFunction}>Log Out</p>
+				</div>
+
+				{/* NOTIFICATION SETTINGS
+				<div className={styles.logoutButtonDiv}>
+					<img src={notifications} alt='notifications'></img>
+					<p>Notifcations Currently: ON</p>
+				</div> */}
+			</div>
+		</>
+	);
 };
