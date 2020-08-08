949eb562f89bf05761cd98a5cedd6f3c4ba202ca
Austin Theriot <austinmtheriot@gmail.com>
2020-08-08 17:17:08 -0500

    Post component CSS rough draft complete

--- presto-web-app/src/Pages/Posts/Posts.js
+++ presto-web-app/src/Pages/Posts/Posts.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Nav from '../../components/Nav/Nav';
+import Post from '../../components/Post/Post';
 
 export default (props) => {
-  return (
-    <>
-      <Nav />
-      <h1>Posts</h1>
-    </>
-  );
+	return (
+		<>
+			<Nav />
+			<h1>Posts</h1>
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+		</>
+	);
 };
