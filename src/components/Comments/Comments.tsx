7e7d382814a166ac2be926695b407061e509d375
Austin Theriot <austinmtheriot@gmail.com>
2020-08-21 00:24:50 -0500

    comments sorted by date now

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 import Comment from '../Comment/Comment';
 
 export default (props: { postId: string; commentsContainer: any }) => {
-	let comments = { ...props.commentsContainer.comments };
+	let commentsObject = { ...props.commentsContainer.comments };
 	let postId = props.postId;
 	let commentsList: any[] = [];
-	if (comments?.count) {
-		for (let key in comments) {
+	console.log(
+		'[Comments]: converting comments object from database into an array'
+	);
+	if (commentsObject?.count) {
+		for (let key in commentsObject) {
 			if (key !== 'count') {
 				//add commentId field to comment (based on docId)
-				let comment = { ...comments[key] }; //copy comment data to prevent TypeError in strict mode (object not extensible)
+				let comment = { ...commentsObject[key] }; //copy comment data to prevent TypeError in strict mode (object not extensible)
 				comment.commentId = key;
 				//add postId field to comment (based on received from props)
 				comment.postId = postId;
 				commentsList.push(comment); //add comments into an array
 			}
 		}
-		commentsList = commentsList.map((comment: any) => (
-			<Comment key={comment!.body! + comment.createdAt} {...comment} />
-		));
+		console.log('[Comments]: sorting comments by date');
+		commentsList = commentsList
+			.sort((postA, postB) => {
+				let a = new Date(postA.createdAt ? postA.createdAt : 0).getTime();
+				let b = new Date(postB.createdAt ? postB.createdAt : 0).getTime();
+				return b - a;
+			})
+			.map((comment: any) => (
+				<Comment key={comment!.body! + comment.createdAt} {...comment} />
+			));
 	}
 
 	return <section>{commentsList}</section>;
