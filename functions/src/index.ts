9506db966eef58e744b253ef4e4011a1bd1d2a1a
Austin Theriot <austinmtheriot@gmail.com>
2020-12-17 13:26:28 -0600

    functions working! renamed functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,11 +5,11 @@ import { updateProfile } from './updateProfile';
 //Update post data:
 //- when a user likes/comments
 //- OR when a user changes their profile info and it has to be updated in posts
-exports.updatePostData = functions.firestore
+exports.updatePost = functions.firestore
 	.document('posts/{postId}/{collectionId}/{docId}') //watch subcollection of likes/comments for changes
 	.onWrite(updatePost);
 
 //Update user's profile info in every post, comment, and like
-exports.updateProfilePicUrl = functions.firestore
+exports.updateProfile = functions.firestore
 	.document('users/{userId}') //watch user docs for changes
 	.onWrite(updateProfile);
